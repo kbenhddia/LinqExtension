100eab81f8f2f1bd8efe9933d32e57f21dc6226c
kbenhddia <benhddia-khalil@live.fr>
2014-03-05 12:55:48 -0800

    Update LinqExtension.cs

--- LinqExtension.cs
+++ LinqExtension.cs
@@ -1,7 +1,8 @@
 /*
  * Licensing :
  * LinqExtension is free.
- * The source code is issued under a permissive free license, which means you can modify it as you please, and incorporate it into your own commercial or non-commercial software. 
+ * LINQExtension is free.
+ * The source code is issued under a permissive free license, which means you can modify it as you like, and incorporate it into your own commercial or non-commercial software.
  * Enjoy!
  */
  
